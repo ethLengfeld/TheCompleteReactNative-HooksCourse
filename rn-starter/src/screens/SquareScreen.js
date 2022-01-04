e582b2ea903d29ffce29bdcc98d3aa9d5406f7f7
EthanLengfeld.Dev@gmail.com <EthanLengfeld.Dev@gmail.com>
2022-01-03 22:33:26 -0600

    finished reducer

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -8,11 +8,14 @@ const CHANGE_VAL = 10;
 const reducer = (state, action) => {
     switch(action.colorToChange) {
       case 'red':
-        return {...state, red: state.red + action.amount };
+        // console.log("changing red");
+        return { ...state, red: state.red + action.amount };
       case 'green':
-        return {...state, green: state.green + action.amount };
+        // console.log("changing green");
+        return { ...state, green: state.green + action.amount };
       case 'blue':
-        return {...state, blue: state.blue + action.amount };
+        // console.log("changing blue");
+        return { ...state, blue: state.blue + action.amount };
       default:
         return state;
     }
@@ -20,23 +23,24 @@ const reducer = (state, action) => {
 
 const SquareScreen = () => {
 
+  // dispatch === run my reducer
   const [ state, dispatch ] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
-
+  const { red, green, blue } = state;
   return (
     <View>
       <ColorCounter
-        onIncrease={() => {}}
-        onDecrease={() => {}}
+        onIncrease={() => dispatch({ colorToChange: 'red', amount: CHANGE_VAL })}
+        onDecrease={() => dispatch({ colorToChange: 'red', amount: -1*CHANGE_VAL })}
         color="Red"
       />
       <ColorCounter
-        onIncrease={() => {}}
-        onDecrease={() => {}}
+        onIncrease={() => dispatch({ colorToChange: 'green', amount: CHANGE_VAL })}
+        onDecrease={() => dispatch({ colorToChange: 'green', amount: -1*CHANGE_VAL })}
         color="Green"
       />
       <ColorCounter
-        onIncrease={() => {}}
-        onDecrease={() => {}}
+        onIncrease={() => dispatch({ colorToChange: 'blue', amount: CHANGE_VAL })}
+        onDecrease={() => dispatch({ colorToChange: 'blue', amount: -1*CHANGE_VAL })}
         color="Blue"
       />
 
@@ -48,6 +52,7 @@ const SquareScreen = () => {
         }}
       >
         <Text>
+          {/* rgb(${state.red},${state.green},${state.blue}) */}
           rgb(${red},${green},${blue})
         </Text>
       </View>
