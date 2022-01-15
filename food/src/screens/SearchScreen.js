3b8b6b6e43487c6178ed6b5b847c038ee070e83f
EthanLengfeld.Dev@gmail.com <EthanLengfeld.Dev@gmail.com>
2022-01-14 22:25:07 -0600

    Added navigator

---
+++ food/src/screens/SearchScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchScreen = () => {
+
+    return (
+        <View>
+            <Text>Welcome to Search Screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default SearchScreen;
