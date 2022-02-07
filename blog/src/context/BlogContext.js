70b01bff07ffd9fd2eb18ca8cd90c116a1b902b6
EthanLengfeld.Dev@gmail.com <EthanLengfeld.Dev@gmail.com>
2022-02-06 19:02:53 -0600

    added edit functionality

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -4,9 +4,25 @@ import createDataContext from "./createDataContext";
 const blogReducer = (state, action) => {
   switch (action.type) {
     case "add_blogpost":
-      return [...state, { id: Math.floor(Math.random() * 999999), title: action.payload.title, content: action.payload.content }];
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 999999),
+          title: action.payload.title,
+          content: action.payload.content,
+        },
+      ];
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
+    case "edit_blogpost":
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
+        // if (blogPost.id === action.payload.id) {
+        //   return action.payload;
+        // } else {
+        //   return blogPost;
+        // }
+      });
     default:
       return state;
   }
@@ -16,21 +32,27 @@ const addBlogPost = (dispatch) => {
   // add callback for api
   return (title, content, callback) => {
     dispatch({ type: "add_blogpost", payload: { title, content } });
-    callback();
+    if(callback) {callback()}
   };
 };
 
 const deleteBlogPost = (dispatch) => {
   return (id) => {
     dispatch({ type: "delete_blogpost", payload: id });
-  }
+  };
 };
 
+const editBlogPost = (dispatch) => {
+  return (id, title, content, callback) => {
+    dispatch({ type: "edit_blogpost", payload: { id, title, content } });
+    if(callback) {callback()}
+  };
+};
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost },
-  [{ title: "TEST Blogpost", content: "This is test content", id: 1}] 
+  { addBlogPost, deleteBlogPost, editBlogPost },
+  [{ title: "TEST Blogpost", content: "This is test content", id: 1 }]
 );
 
 // const BlogContext = React.createContext();
--- blog/src/screens/EditScreen.js
+++ blog/src/screens/EditScreen.js
@@ -1,37 +1,37 @@
-import React, { useContext } from 'react';
-import { StyleSheet } from 'react-native';
-import BlogPostForm from '../components/BlogPostForm';
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
+import BlogPostForm from "../components/BlogPostForm";
 import { Context } from "../context/BlogContext";
 
 const EditScreen = ({ navigation }) => {
-    const { state } = useContext(Context);
-    const blogPost = state.find(
-      (blogPost) => blogPost.id === navigation.getParam("id")
-    );
+  const id = navigation.getParam("id");
+  const { state, editBlogPost } = useContext(Context);
+  const blogPost = state.find((blogPost) => blogPost.id === id);
 
-    return (
-        <BlogPostForm 
-        initialValues={{ title: blogPost.title, content: blogPost.content }}
-        onSubmit={(title, content) => {
-
-        }}/>
-    );
+  return (
+    <BlogPostForm
+      initialValues={{ title: blogPost.title, content: blogPost.content }}
+      onSubmit={(title, content) => {
+        editBlogPost(id, title, content, () => navigation.pop());
+      }}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
-    textInput: {
-        fontSize: 18,
-        borderWidth: 1,
-        borderColor: 'black',
-        padding: 3,
-        margin: 10,
-    },
-    label: {
-        fontSize: 22,
-        fontWeight: 'bold',
-        marginBottom: 10,
-        marginLeft: 10
-    }
+  textInput: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 3,
+    margin: 10,
+  },
+  label: {
+    fontSize: 22,
+    fontWeight: "bold",
+    marginBottom: 10,
+    marginLeft: 10,
+  },
 });
 
 export default EditScreen;
