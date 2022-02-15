557369f07e308543e39a45464a4c7a20d517a77c
EthanLengfeld.Dev@gmail.com <EthanLengfeld.Dev@gmail.com>
2022-02-14 20:43:55 -0600

    completed blog app

--- blog/src/api/jsonServer.js
+++ blog/src/api/jsonServer.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://cdb2-67-163-28-157.ngrok.io'
+    baseURL: 'http://bcbc-67-163-28-157.ngrok.io'
 });
--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -5,15 +5,15 @@ const blogReducer = (state, action) => {
   switch (action.type) {
     case "get_blogpost":
       return action.payload;
-    case "add_blogpost":
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 999999),
-          title: action.payload.title,
-          content: action.payload.content,
-        },
-      ];
+    // case "add_blogpost":
+    //   return [
+    //     ...state,
+    //     {
+    //       id: Math.floor(Math.random() * 999999),
+    //       title: action.payload.title,
+    //       content: action.payload.content,
+    //     },
+    //   ];
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
     case "edit_blogpost":
@@ -37,7 +37,8 @@ const getBlogPost = (dispatch) => {
   };
 };
 
-const addBlogPost = (dispatch) => {
+const addBlogPost = () => {
+// const addBlogPost = (dispatch) => {
   // add callback for api
   return async (title, content, callback) => {
     await jsonServer.post('/blogposts', { title, content });
@@ -48,13 +49,16 @@ const addBlogPost = (dispatch) => {
 };
 
 const deleteBlogPost = (dispatch) => {
-  return (id) => {
+  return async (id) => {
+    await jsonServer.delete(`/blogposts/${id}`);
+    
     dispatch({ type: "delete_blogpost", payload: id });
   };
 };
 
 const editBlogPost = (dispatch) => {
-  return (id, title, content, callback) => {
+  return async (id, title, content, callback) => {
+    await jsonServer.put(`blogposts/${id}`, { title, content });
     dispatch({ type: "edit_blogpost", payload: { id, title, content } });
     if(callback) {callback()}
   };
