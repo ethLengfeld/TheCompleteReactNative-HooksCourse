132c7050c1d059087c43e34041d4320e7adbac67
EthanLengfeld.Dev@gmail.com <EthanLengfeld.Dev@gmail.com>
2022-03-08 21:34:16 -0600

    added context functions

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -1,14 +1,36 @@
 import createDataContext from "./createDataContext";
 
 const authReducer = (state, action) => {
-    switch (action.type) {
-        default:
-            return state;
-    }
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const signup = (dispatch) => {
+  return ({ email, password }) => {
+    // make api request to sign up with email and password
+    // if we sign up, modify our state, and say that we are authenticated
+    // if signing up fails, we probably need to reflect error
+  };
+};
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {
+    // Try to signin
+    // Success update state
+    // Error show message
+  };
+};
+
+const signout = (dispatch) => {
+  return () => {
+    // sign out, update state
+  };
 };
 
 export const { Provider, Context } = createDataContext(
-    authReducer,
-    {},
-    { isSignedIn: false }
+  authReducer,
+  { signup, signin, signout },
+  { isSignedIn: false }
 );
