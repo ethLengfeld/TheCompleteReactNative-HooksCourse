2a16a77b628b6f43b15a8994ab99f930214a212e
EthanLengfeld.Dev@gmail.com <EthanLengfeld.Dev@gmail.com>
2022-03-23 22:32:15 -0500

    added asyc storage retrieval sign in

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import AuthForm from "../components/AuthForm";
 import { Context as AuthContext } from "../context/AuthContext";
@@ -6,8 +6,11 @@ import NavLink from "../components/NavLink";
 import { NavigationEvents } from "react-navigation";
 
 const SignupScreen = ({ navigation }) => {
-  const { state, signup, clearErrorMessage } = useContext(AuthContext);
+  const { state, signup, clearErrorMessage, tryLocalSignin } = useContext(AuthContext);
 
+  useEffect(() => {
+    tryLocalSignin();
+  }, []);
   return (
     <View style={styles.container}>
       {/* Called anytime we navigate to this component */}
